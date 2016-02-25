f7b3bf6f7471dff761429296a22a6024f228da78
sandipan96 <sandipanganguly96@gmail.com>
2016-02-25 16:00:53 +0000

    Added files via upload

--- tasksheet 4/calcPos.c
+++ tasksheet 4/calcPos.c
@@ -3,8 +3,11 @@
 #include <math.h>
 #include <unistd.h>
 #include "dimensions.h"
+#define M_PI 3.1415
 
 int DO = 22, CLK = 23, DI = 24, CS = 25; 
+float total_distance;
+	float angleTurned;
 
 struct location{
 	float x;
@@ -28,8 +31,8 @@ void calcPosition(){
 	float difference;
 	float wheelRevCount;
 	int l_encoder = 0, r_encoder = 0;
-	float total_distance;
-	float angleTurned;
+  float rad_turn;
+	
 	
 	sd_mount(DO, CLK, DI, CS);
 	FILE* fp = fopen("Position.text", "w");
@@ -37,8 +40,8 @@ void calcPosition(){
 	wheelRevCount = (M_PI * WHEEL_DIAMETER_CM)/360.0;
 	
 	drive_getTicks(&l_encoder,&r_encoder);
-	l_dist = (float)l_encoder * rev_count;
-	r_dist = (float)r_encoder * rev_count;
+	l_dist = (float)l_encoder * wheelRevCount;
+	r_dist = (float)r_encoder * wheelRevCount;
 	
 	current_cos = cos(current_pos.theta);
 	current_sin = sin(current_pos.theta);
@@ -52,8 +55,8 @@ void calcPosition(){
 		rad_turn = ROBOT_WIDTH_CM * (l_dist + r_dist)/2.0/(l_dist - r_dist);
 		difference = r_dist - l_dist;
 		
-		current_pos.x += rad_turn * (sin(differnce / ROBOT_WIDTH_CM + current_pos.theta)-current_sin);
-		current_pos.y += rad_turn * (cos(differnce / ROBOT_WIDTH_CM + current_pos.theta)-current_cos);
+		current_pos.x += rad_turn * (sin(difference / ROBOT_WIDTH_CM + current_pos.theta)-current_sin);
+		current_pos.y += rad_turn * (cos(difference / ROBOT_WIDTH_CM + current_pos.theta)-current_cos);
 		
 		current_pos.theta += difference / ROBOT_WIDTH_CM;
 		
@@ -67,15 +70,15 @@ void calcPosition(){
 	
 	}
 	total_distance = sqrt(pow(current_pos.x,2)+pow(current_pos.y,2));
-	fwrite(&total_distance, sizeof(total_distance, 1, fp));
+	fwrite(&total_distance, sizeof(total_distance), 1, fp);
 	angleTurned = current_pos.theta * 57.29578;
-	fwrite(&angleTurned, sizeof(angleTurned, 1, fp));
+	fwrite(&angleTurned, sizeof(angleTurned), 1, fp);
 	fclose(fp);
 	
 	fp = fopen("Position.text", "r");
-	fread = (&total_distance, 7, 1, fp);
+	fread(&total_distance, 5, 1, fp);
 	print("distance travelled is %.2f\n",total_distance);
-	fread = (&angleTurned, 7, 1, fp);
+	fread(&angleTurned, 5, 1, fp);
 	print("distance travelled is %.2f\n",angleTurned);
 	fclose(fp);
 	
